e3f03364f51fd4b62a81d5dd10b299a45cd2554a
Bingwei <chenbwei2012@gmail.com>
2016-11-18 10:20:19 +0800

    delete some useless codes

--- src/essfwi/essfwiframework.cpp
+++ src/essfwi/essfwiframework.cpp
@@ -138,9 +138,7 @@ void calgradient(const Damp4t10d &fmMethod,
 
   for(int it=0; it<nt; it++) {
     fmMethod.addSource(&sp1[0], &encSrc[it * ns], allSrcPos);
-    //printf("it = %d, forward 1\n", it);
     fmMethod.stepForward(&sp0[0], &sp1[0]);
-    //printf("it = %d, forward 2\n", it);
     std::swap(sp1, sp0);
     fmMethod.writeBndry(&bndr[0], &sp0[0], it);
   }
@@ -151,30 +149,21 @@ void calgradient(const Damp4t10d &fmMethod,
   for(int it = nt - 1; it >= 0 ; it--) {
     fmMethod.readBndry(&bndr[0], &sp0[0], it);
     std::swap(sp0, sp1);
-    //printf("it = %d, back 1\n", it);
     fmMethod.stepBackward(&sp0[0], &sp1[0]);
-    //printf("it = %d, back 2\n", it);
     fmMethod.subEncodedSource(&sp0[0], &encSrc[it * ns]);
 
     /**
      * forward propagate receviers
      */
     fmMethod.addSource(&gp1[0], &vsrc_trans[it * ng], allGeoPos);
-    //printf("it = %d, receiver 1\n", it);
     fmMethod.stepForward(&gp0[0], &gp1[0]);
-    //printf("it = %d, receiver 2\n", it);
     std::swap(gp1, gp0);
 
     if (dt * it > 0.4) {
-      //printf("it = %d, cross 1\n", it);
       cross_correlation(&sp0[0], &gp0[0], &g0[0], g0.size(), 1.0);
-      //printf("it = %d, cross 2\n", it);
     } else if (dt * it > 0.3) {
-      //printf("it = %d, cross 3\n", it);
       cross_correlation(&sp0[0], &gp0[0], &g0[0], g0.size(), (dt * it - 0.3) / 0.1);
-      //printf("it = %d, cross 4\n", it);
     } else {
-      //printf("it = %d, cross 5\n");
       break;
     }
  }
